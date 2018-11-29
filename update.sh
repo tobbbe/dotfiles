bc1bb9e22fa12970f2faa744168d6c58680ed261
Tobias Ekman <tobias@toxic.se>
2018-11-29 15:24:46 +0100

    updatefix

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-# run with `sh setup.s`
+# run with `sh setup.s` OR use `reload` in terminal
 
 # This may overwrite existing files in your home directory
 rsync --exclude ".git/" \
@@ -6,4 +6,4 @@ rsync --exclude ".git/" \
 		--exclude ".osx" \
 		--exclude "update.sh" \
 		--exclude "README.md" \
-		-avh --no-perms . ~;
+		-avh --no-perms ~/dotfiles/ ~; # MAKE SURE dotfiles is at ~/dotfiles, else use . and execute this in this folder

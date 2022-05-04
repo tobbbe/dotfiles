c15965be8dd3b85074cfdb42419d1fd4b4823876
Tobias Ekman <tobekman@gmail.com>
2022-05-04 11:52:36 +0200

    handle inputs correctly

--- scripts/devpaths.sh
+++ scripts/devpaths.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 ## link to this in alfred "script filter" like this: `~/.dotfiles/scripts/devpaths.sh $@`
-## For some reason the first line is excluded by jq, thats why I prepend "argh"
+## everytime 'inputs' is called it pops from the list https://stackoverflow.com/questions/72110455/why-is-the-first-line-is-skipped/72110653#72110653
 ## https://juripakaste.fi/jq-alfred-script-filter/
 
 extraPaths="/Users/tobbbe/.dotfiles" # just add new line inside string here to add more items
 
-(echo "argh" && find ~/dev ~/devp -mindepth 1 -maxdepth 1 -type d && echo $extraPaths) | /usr/local/bin/jq -nR \
+(find ~/dev ~/devp -mindepth 1 -maxdepth 1 -type d && echo $extraPaths) | /usr/local/bin/jq -nR \
 '{
     # things here is jq-things! split() etc NOT bash.
     # Wrap in '' to use args. ex see contains below
     "items": [
-        inputs |
-        select(length>0) |                                          # remove empty lines
         inputs as $path |
-        $path | split("/")[-1] as $title |                          # take dir name. `($path | split("/")[-1] | sub("^\\."; "")) as $title |` to remove leading dot
+        $path | split("/")[-1] as $title |
         select($title | contains("'$1'") and contains("'$2'") and contains("'$3'")) |
         {
             "uid": $path,

3c18bc1f5354645e79187874fb182881153e94fe
Tobias Ekman <tobekman@gmail.com>
2023-03-30 12:07:21 +0200

    no message

--- scripts/js-test.js
+++ scripts/js-test.js
@@ -2,11 +2,11 @@
 
 // Required parameters:
 // @raycast.schemaVersion 1
-// @raycast.title js test
+// @raycast.title Info
 // @raycast.mode fullOutput
 
 // Optional parameters:
-// @raycast.icon 🤖
+// @raycast.icon 👨‍🔬
 
 // @raycast.author Tobbbe
 
@@ -22,14 +22,36 @@ const FgWhite = '\x1b[37m';
 
 ;(async () => {
   try {
-    const numPart = `# 22: `
-    const titlePart = FgYellow + "Title" + ` | ${FgRed}PR # ${FgCyan}${42}` + '\n'
-    const urlPart = '     ' + 'https://google.com' + '\n\n'
+    // const numPart = `# 22: `
+    // const titlePart = FgYellow + "Title" + ` | ${FgRed}PR # ${FgCyan}${42}` + '\n'
+    // const urlPart = '     ' + 'https://google.com' + '\n\n'
   
-    const row = numPart + titlePart + urlPart
+    // const row = numPart + titlePart + urlPart
+    // process.stdout.write(row)
 
-    process.stdout.write(row)
+    process.stdout.write(`
+
+    Week: ${getCurrentWeekNr()}
+
+    `.trim() + '\n\n\n\n')
+
   } catch (err) {
     console.error(err)
   }
 })()
+
+function getCurrentWeekNr() {
+  const today = new Date();
+  const weekNumber = getISOWeek(today);
+
+  return weekNumber;
+
+  // Function to calculate the week number based on the ISO 8601 standard
+  function getISOWeek(date) {
+    const dayOfWeek = (date.getDay() + 6) % 7; // Adjust for Sunday being 0
+    const january4 = new Date(date.getFullYear(), 0, 4);
+    const startOfWeek = new Date(january4.setDate(january4.getDate() - january4.getDay() + 1));
+    const weekNumber = Math.ceil(((date - startOfWeek) / 86400000 + 1) / 7);
+    return weekNumber;
+  }
+}

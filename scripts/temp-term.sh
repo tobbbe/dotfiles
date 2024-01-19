08c6bc34f821394cbb60b1c3d8d895ee2a0b138f
Tobias Ekman <tobekman@gmail.com>
2024-01-19 08:02:47 +0100

    no message

---
+++ scripts/temp-term.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Temp term
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ⬛
+
+osascript -e 'tell application "iTerm" to create window with default profile'

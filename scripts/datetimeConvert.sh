d9d9dd2e5a5d5bc5a5764fe530663b36d53c1073
Tobias Ekman <tobekman@gmail.com>
2025-03-19 14:51:37 +0100

    no message

--- scripts/datetimeConvert.sh
+++ scripts/datetimeConvert.sh
@@ -12,13 +12,15 @@
 
 # @raycast.author Tobbbe
 
-node -e "console.table(
-  [
-    [
-      'UTC-0', new Date($1).toLocaleString('sv-SE', { timeZone: 'UTC' })
-    ],
-    [
-      'Sweden time', new Date($1).toLocaleString('sv-SE', { timeZone: 'Europe/Stockholm' })
-    ]
-  ]
-)"
+# node -e "console.table(
+#   [
+#     [
+#       'UTC-0', new Date($1).toLocaleString('sv-SE', { timeZone: 'UTC' })
+#     ],
+#     [
+#       'Sweden time', new Date($1).toLocaleString('sv-SE', { timeZone: 'Europe/Stockholm' })
+#     ]
+#   ]
+# )"
+
+node -e "console.info('Sweden ' + new Date($1).toLocaleString('sv-SE', { timeZone: 'Europe/Stockholm' }), '\n\nUTC-0  ' + new Date($1).toLocaleString('sv-SE', { timeZone: 'UTC' }))"

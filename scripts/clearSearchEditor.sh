8c1b5a248bd69db218a89c0e41aa1df30a28e104
Tobias Ekman <tobekman@gmail.com>
2025-03-26 10:30:42 +0100

    nice

--- scripts/clearSearchEditor.sh
+++ scripts/clearSearchEditor.sh
@@ -4,4 +4,4 @@
 # Files with osascript doesnt seems to work well with raycast scripts
 # So its better to use with Karabiner, because then you can use 'frontmost_application_if'
 
-osascript -e 'tell application "System Events"' -e 'key code 53' -e 'key code 53' -e 'keystroke "e" using {command down}' -e 'keystroke "1" using {command down}' -e 'end tell'
+osascript -e 'tell application "System Events"' -e 'keystroke "1" using {command down}' -e 'keystroke "b" using {command down, option down, control down}' -e 'end tell'

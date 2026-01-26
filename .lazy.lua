3317551d7cc5dba6419604f5fc632b4291d7e794
Tobias Ekman <tobekman@gmail.com>
2026-01-26 09:01:45 +0100

    ok

---
+++ .lazy.lua
@@ -0,0 +1,20 @@
+vim.g.autoformat = true
+
+-- Disable autoformat for Karabiner config
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "*/karabiner/karabiner.json",
+	callback = function()
+		vim.b.autoformat = false
+	end,
+})
+
+return {
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				json = { "prettierd" },
+			},
+		},
+	},
+}

3b58e7d2afb47655f2c0053fd37545836f9f6adb
Tobias Ekman <tobekman@gmail.com>
2026-04-02 15:10:21 +0200

    ok

--- .config/sketchybar/plugins/spotify.sh
+++ .config/sketchybar/plugins/spotify.sh
@@ -5,7 +5,7 @@ INFO=$(osascript -e '
     if it is running and player state is playing then
       set trackName to name of current track
       set artistName to artist of current track
-      return trackName & " - " & artistName
+      return artistName & " - " & trackName
     else
       return ""
     end if
